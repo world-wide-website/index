48c6c3f36e4f7d11a2d52b931f844cd8ce9a656d
Prudhvi Raj <157046388+world-wide-website@users.noreply.github.com>
2024-09-02 05:51:43 +0530

    Create links.js

---
+++ links.js
@@ -0,0 +1,71 @@
+const Links = [
+ { name: "You tube", link: "https://www.youtube.com/" },
+ { name: "Tools", link: "https://123apps.com/#google_vignette" },
+ { name: "Ibomma", link: "https://ibomma.online/telugu-movies/" },
+ { name: "SVG", link: "https://editor.method.ac/" },
+ { name: "vector veteezy", link: "https:veteezy.com/editor/random" },
+ { name: "vector vectr", link: "https:vectr.com/codepen/welcome" },
+ { name: "vector janvas", link: "https:janvas.com" },
+ { name: "Free Convert", link: "https://www.freeconvert.com/webp-to-jpg/" },
+ { name: "Bing Copilot", link: "https://www.bing.com/chat?q=Microsoft%20Copilot&qs=ds&form=ATCVAJ" },
+ { name: "Unspash Images", link: "https://unsplash.com/" },
+ { name: "pixabay Images", link: "https://pixabay.com/" },
+ { name: "canva", link: "https://www.canva.com/" },
+ { name: "open ai", link: "https://platform.openai.com/docs/overview" },
+ { name: "CV", link: "https://www.goodcv.com/" },
+ { name: "Github", link: "https://github.com/" },
+ { name: "COC layout", link: "https://clashofclans-layouts.com/" },
+ { name: "wynk", link: "https://wynk.in/music" },
+ { name: "W3", link: "https://www.w3schools.com/" },
+ { name: "", link: "" },
+];
+ 
+const icons = "material-symbols-outlined";
+const linkBox = document.createElement("div");
+linkBox.className = "linkbox";
+document.body.appendChild(linkBox);
+
+const explore = document.createElement("a");
+explore.className = icons+" explore";
+explore.textContent = "explore";
+explore.href ="https://www.google.co.in/maps/";
+explore.style.textDecoration = "none";
+explore.style.color = "white";
+linkBox.appendChild(explore);
+
+const scrollbar = document.createElement("div");
+scrollbar.className = "scroll";
+linkBox.appendChild(scrollbar);
+
+for (let i = 0; i < Links.length; i++) {
+ const TabName = Links[i].name;
+ const TabLink = Links[i].link;
+
+  if (TabName && TabLink) { // Check if both name and link are not empty
+   const linkTitle = document.createElement("a");
+   const titleLink = document.createElement("button");
+   linkTitle.className = "linktitle";
+   titleLink.className = "titlelink";
+   
+   titleLink.textContent = TabName //.charAt(0).toUpperCase() + TabName.slice(1).toLowerCase();
+   linkTitle.href = TabLink;
+   
+   scrollbar.appendChild(linkTitle);
+   linkTitle.appendChild(titleLink);
+  }
+}
+
+const more = document.createElement("div");
+ more.className = "more";
+ more.textContent = "more";
+ linkBox.appendChild(more);
+
+const downarrow = document.createElement("span");
+ downarrow.className = icons;
+ downarrow.textContent = "expand_more";
+ more.appendChild(downarrow);
+ 
+// Add the click event listener
+more.addEventListener('click', function() {
+  scrollbar.classList.toggle('expanded');
+});
